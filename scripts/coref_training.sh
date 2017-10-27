7918fe0d0299def07fad082f1742454841bee779
Jankus1994 <faryadj@seznam.cz>
2017-10-27 21:38:20 +0200

    Add files via upload

--- scripts/coref_training.sh
+++ scripts/coref_training.sh
@@ -3,4 +3,5 @@
 export PATH=../bin:$PATH
 export PYTHONPATH=../:$PYTHONPATH
 
-udapy read.Conllu files='!*.out.conllu' demo.Coreference.CoNLL.Conll_selector | python ../udapi/block/demo/Coreference/Other/trainer.py model.txt
+udapy read.Conllu files='!train/*.out.conllu' demo.Coreference.CoNLL.Conll_training_selector > vectors.txt
+python3 ../udapi/block/demo/Coreference/Other/trainer.py $1 vectors.txt
