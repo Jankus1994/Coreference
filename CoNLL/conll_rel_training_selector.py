6048e467fd8b261e39b86829b6e1dc0b8dae2016
Jankus1994 <faryadj@seznam.cz>
2018-05-02 19:51:02 +0200

    Delete conll_rel_training_selector.py

--- CoNLL/conll_rel_training_selector.py
+++
@@ -1,9 +0,0 @@
-# Jan Faryad
-# 18. 11. 2017
-#
-
-from udapi.block.demo.Coreference.CoNLL.conll_rel_selector import Conll_rel_selector
-
-class Conll_rel_training_selector( Conll_rel_selector):
-    def for_training( self):
-        return True
