cfe7a39610d32e762ce9726f7dabc7171cd9fe1b
Jankus1994 <faryadj@seznam.cz>
2018-05-02 20:05:39 +0200

    New version

--- PDT/pdt_coreferent.py
+++ PDT/pdt_coreferent.py
@@ -1,5 +1,7 @@
-# Jan Faryad]
+# Jan Faryad
 # 12. 7. 2017
+#
+# Class representing a corefering word in the text. At the beginning, the coreference is described with PDT IDs, later with CoNLL-U nodes and at the end with a cluster number
 
 class Pdt_coreferent():
     def __init__( self, own_dropped, own_ID, coref_dropped, coref_ID):
@@ -12,4 +14,11 @@ class Pdt_coreferent():
         self.own_node = None
         self.coref_node = None
         
+        self.coreferents_in_cluster = [] # Pdt_coreferents corefering with this one
         self.cluster_ID = -1
+
+    
+    def set_own_node( self, node):
+        self.own_node = node
+    def set_coref_node( self, node):
+        self.coref_node = node
